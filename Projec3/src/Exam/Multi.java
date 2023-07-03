802d217590849b59f12e1be9eb86b923600c350d
Nithya Shree <Nithya Shree@192.168.40.42>
2023-07-03 23:50:14 +0530

    push

---
+++ Projec3/src/Exam/Multi.java
@@ -0,0 +1,12 @@
+package Exam;
+
+public class Multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=78,b=76,mul;
+		mul=a*b;
+		 System.out.println("subtraction is" +mul);
+	}
+
+}
